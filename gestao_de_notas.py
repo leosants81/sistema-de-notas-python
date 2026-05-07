298f37b36d24ffcf5dfd268c70b718f6cfc683dc
Leonardo Santos <leosants81@gmail.com>
2026-05-07 13:45:45 -0300

    melhorando sistema de notas com POO e tratamento de erros

--- gestao_de_notas.py
+++ gestao_de_notas.py
@@ -1,36 +1,33 @@
-# ATIVIDADE PROPOSTA: 
-# Você foi contratado para desenvolver um sistema simples de gestão de notas de alunos. O sistema 
-# deve permitir que o usuário adicione notas, calcule a média das notas, determine a situação do 
-# aluno (aprovado ou reprovado), e exiba um relatório final. Utilize estruturas condicionais, de 
-# repetição e funções. 
-# Cadastro de Notas: 
-# • O sistema deve permitir que o usuário insira as notas dos alunos. 
-# • As notas devem ser armazenadas em uma lista. 
-# Cálculo da Média: 
-# • O sistema deve calcular a média das notas inseridas. 
-# Determinação da Situação: 
-# • Se a média for maior ou igual a 7, o aluno está aprovado. 
-# • Se a média for menor que 7, o aluno está reprovado. 
+# ATIVIDADE PROPOSTA:
+# Você foi contratado para desenvolver um sistema simples de gestão de notas de alunos. O sistema
+# deve permitir que o usuário adicione notas, calcule a média das notas, determine a situação do
+# aluno (aprovado ou reprovado), e exiba um relatório final. Utilize estruturas condicionais, de
+# repetição e funções.
+# Cadastro de Notas:
+# • O sistema deve permitir que o usuário insira as notas dos alunos.
+# • As notas devem ser armazenadas em uma lista.
+# Cálculo da Média:
+# • O sistema deve calcular a média das notas inseridas.
+# Determinação da Situação:
+# • Se a média for maior ou igual a 7, o aluno está aprovado.
+# • Se a média for menor que 7, o aluno está reprovado.
 # Relatório Final:
-# Exibir as notas inseridas, a média e a situação do aluno. 
+# Exibir as notas inseridas, a média e a situação do aluno.
 
 class Aluno:
     def __init__(self, nome):
-        self.nome = nome 
-        self.notas = [] 
-
+        self.nome = nome
+        self.notas = []
 
     def adicionar_nota(self, nota):
         if nota < 0 or nota > 10:
             raise ValueError('Nota deve ser entre 0 e 10')
         self.notas.append(nota)
 
-
     def calcular_media(self):
         if not self.notas:
             return 0
         return sum(self.notas) / len(self.notas)
-        
 
     def situacao(self):
         media = self.calcular_media()
@@ -39,21 +36,40 @@ class Aluno:
         else:
             return "o aluno esta reprovado"
 
+    def relatorio(self):
+        print(f'Aluno: {self.nome}')
+        print()
+        print(f'Notas: {self.notas}')
+        print()
+        print(f'Média: {self.calcular_media():.2f}')
+        print()
+        print(f'Situação: {self.situacao()}')
 
 
-
-aluno = Aluno("Léo")
-print(f'Aluno: {aluno.nome}')
-
-try:
-    aluno.adicionar_nota(8)
-    aluno.adicionar_nota(7.8)
-    aluno.adicionar_nota(10) 
-
-except ValueError as e:
-    print(f'Erro capturado: {e}')
+nome = input('digite seu nome:')
+aluno = Aluno(nome)
 
 
-print(f'Notas: {aluno.notas}')
-print(f'Média: {aluno.calcular_media():.2f}')
-print(f'Situação: {aluno.situacao()}')
+while True:
+    try:
+        quantidade = int(input('digite a quantidade de notas: '))
+        if quantidade <= 0:
+            print('A quantidade deve ser maior que zero')
+            continue
+        break
+    except ValueError:
+        print('valor invalido digite numeros inteiros')
+
+for i in range(quantidade):
+    while True:
+        try:
+            nota = float(input(f"Digite a {i+1}ª nota: "))
+
+            aluno.adicionar_nota(nota)
+
+            break
+
+        except ValueError as erro:
+            print(f"Erro: {erro}")
+
+aluno.relatorio()
